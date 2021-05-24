94a15f7f41865995094b6127013daba61d1d64d2
Linbudu <48507806+linbudu599@users.noreply.github.com>
2021-05-24 16:26:49 +0800

    :sparkles: feat(code): update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import Home from '@/pages/home';
+
+import AvatarURL from './assets/avatar.jpg';
 
 interface IApp {
   name: string;
 }
 
+const StyledButton = styled.button`
+  text-align: center;
+  width: 190px;
+  height: 45px;
+  font-size: 20px;
+  border-radius: 6px;
+  background-color: steelblue;
+  color: white;
+  border: 0px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CountPara = styled.p`
+  text-align: center;
+  font-size: 32px;
+  color: salmon;
+`;
+
+const AvatarContainer = styled.div`
+  display: flex;
+
+  justify-content: center;
+  margin-bottom: 25px;
+`;
+
+const Avatar = styled.img`
+  width: 350px;
+  height: 350px;
+  border-radius: 175px;
+`;
+
 const App: React.FC<IApp> = ({ name }) => {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {}, []);
+
   return (
     <>
-      <p className="favor" style={{ textAlign: 'center' }}>
-        Hi! {name}
-      </p>
+      <h1 style={{ textAlign: 'center' }}>Hi! {name}</h1>
+      <CountPara>{count}</CountPara>
+      <AvatarContainer>
+        <Avatar src={AvatarURL} />
+      </AvatarContainer>
+      <StyledButton
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        Click to add count!
+      </StyledButton>
+
       <Home />
     </>
   );
